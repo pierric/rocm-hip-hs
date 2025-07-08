ddf7a3918383ca91fd8ab9d437928842b82824b6
Jiasen Wu <jiasenwu@hotmail.com>
2025-07-08 05:33:27 +0200

    update tests

--- test/MemSpec.hs
+++ test/MemSpec.hs
@@ -1,5 +1,7 @@
 module MemSpec where
 
+import Control.Monad
+import Text.Printf
 import Test.Hspec
 import ROCm.HIP
 
@@ -15,11 +17,21 @@ spec = do
       hipFree ptr
 
   describe "array" $ do
-    it "allocate array valid" $ do
-      -- not documented at all, but valid num_channels are 1,2,4 only
-      hipArrayCreate (HipArrayDescriptor 32 32 4 HipAdFormatFloat)
-      return ()
+    let asInt a = fromIntegral a :: Int
+    let size = [8,128,1024]
+    -- not documented at all, but valid num_channels are 1,2,4 only
+    let chnl = [1,2,4]
+    let fmt  = [HipAdFormatSignedInt8, HipAdFormatUnsignedInt8, HipAdFormatHalf, HipAdFormatFloat]
+    forM_ [(s, c, f) | s<-size, c<-chnl, f<-fmt] $ \(s,c,f) -> do
+      let title = printf "allocate array %dx%dx%d %s"
+                         (asInt s) (asInt s) (asInt c) (show f)
+      it title $ do
+        void $ hipArrayCreate (HipArrayDescriptor s s c f)
 
-    it "allocate array w/ bad dim" $ do
-      hipArrayCreate (HipArrayDescriptor 32 32 3 HipAdFormatFloat) `shouldThrow` anyErrorCall
+    let chnl_bad = [0,3,5]
+    forM_ [(s, c, f) | s<-size, c<-chnl_bad, f<-fmt] $ \(s,c,f) -> do
+      let title = printf "allocate array (bad dim) %dx%dx%d %s"
+                         (asInt s) (asInt s) (asInt c) (show f)
+      it title $ do
+        hipArrayCreate (HipArrayDescriptor s s c f) `shouldThrow` anyErrorCall
 
