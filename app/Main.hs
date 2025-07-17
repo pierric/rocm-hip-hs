3c8123568b3d3bf5c20a2d5fcf160b8c3a6433a7
Jiasen Wu <jiasenwu@hotmail.com>
2025-07-17 15:01:40 +0200

    compile the kernel in test

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,31 @@ import Foreign.Ptr
 import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable.Mutable as MV
 import qualified Data.Vector.Storable as V
+import Control.Monad (when)
+import System.Posix.Files (fileExist)
 import ROCm.HIP
+
+kernelFile :: String
+kernelFile = "test/saxpy.hsaco"
 
 main :: IO ()
 main = do
+  exist <- fileExist kernelFile
+  when (not exist) $
+    error $ "Kernel file '" ++ kernelFile ++ "' doesn't exist. Compile it with the command: \n" ++
+      "  hipcc -O3 --genco --offload-arch=gfx1100 test/saxpy.hip -o test/saxpy.hsaco"
+
   let size = 1000
   let size_bytes = size * 4 :: CSize
   let block_size = 256 :: CUInt
   let grid_size = (fromIntegral size + 255) `div` block_size
 
+  -- input 1: 1,2,3,4, ...
+  -- input 2: 1,3,5,7, ...
   let hx = V.iterateN (fromIntegral size) (+1) 1 :: Vector Float
   let hy = V.iterateN (fromIntegral size) (+2) 1 :: Vector Float
 
-  mod <- hipModuleLoad "test/saxpy.hsaco"
+  mod <- hipModuleLoad kernelFile 
   fun <- hipModuleGetFunction mod "saxpy"
 
   withHipDeviceMem size_bytes $ \dx ->
@@ -31,5 +43,5 @@ main = do
       out <- MV.new (fromIntegral size)
       MV.unsafeWith out $ \pout -> hipMemcpyDtoH (castPtr pout) dy size_bytes
       out <- V.freeze out :: IO (Vector Float)
-      -- expected result: 3, 7, 11, 15, 19, ...
+      -- expected output: 3, 7, 11, 15, 19, ...
       print $ V.take 10 out
--- test/ModuleSpec.hs
+++ test/ModuleSpec.hs
@@ -8,10 +8,23 @@ import Foreign.Ptr
 import Foreign.Storable
 import Text.Printf
 import Test.Hspec
+import System.Posix.Files (fileExist)
+import System.Process (callCommand)
 import ROCm.HIP
 
+kernelFile :: String
+kernelFile = "test/saxpy.hsaco"
+
 spec :: Spec
-spec = do
+spec = beforeAll_ ensureKernelCompiled $ do
   describe "module" $ do
     it "load" $ do
-      void $ hipModuleLoad "/home2/jiasen/workspace/hip-hs/rocm-hip/test/saxpy.hsaco"
+      void $ hipModuleLoad kernelFile
+
+ensureKernelCompiled :: IO ()
+ensureKernelCompiled = do
+  exist <- fileExist kernelFile
+  when (not exist) $ do
+    callCommand "hipcc -O3 --genco --offload-arch=gfx1100 test/saxpy.hip -o test/saxpy.hsaco"
+  
+
